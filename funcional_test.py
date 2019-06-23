906c7affd5fbc16b2ced93259f975ede2741df38
kimsunoh <rlatjsdh7902@gmail.com>
2019-06-23 19:10:21 +0900

    Add unittest

--- funcional_test.py
+++ funcional_test.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_웹페이지_정상동작_확인(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('테스트 끝')
+
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
